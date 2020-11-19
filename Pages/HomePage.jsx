1a4652b77c9fcc5b720e8f2193950480c57a9334
manoharkharvi43 <manoharkharvi43@gmail.com>
2020-11-19 13:27:48 +0530

    commit

---
+++ Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HomePage() {
+    return (
+    
+            <Text>This is the Home Page</Text>
+       
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Login_page from './Components/Login_page'
+
+
+export default function Root() {
+    return (
+       <Login_page/>
+    )
+}
+
+const styles = StyleSheet.create({})
